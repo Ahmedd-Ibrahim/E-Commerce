7e9a96d14c68d63c121a48532c68617ff8a7f27d
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2019-06-25 02:57:31 +0200

    item update

--- item.php
+++ item.php
@@ -16,14 +16,14 @@ if(isset($_SESSION['user'])){
 <div class="container">
 <?php 
     if( isset($_GET['do']) && $_GET['do'] == 'item'){
+        if(! empty(getItem('item_id', $_GET['id']))){
+            foreach(getItem('item_id', $_GET['id']) as $userAds){
     ?>
 <div class="panel panel-primary">
   
-            <div class="panel-heading">My ADs</div>
+            <div class="panel-heading text-center"><?php echo $userAds['name'] ?></div>
             <div class="panel-body">
                 
-                <?php if(! empty(getItem('item_id', $_GET['id']))){
-                foreach(getItem('item_id', $_GET['id']) as $userAds){?>
                      <div class="col-sm-12 col-md-12">
                 <div class="thumbnail item-box">
                     <span class="price"> <?php echo $userAds['price'] ?></span>
@@ -41,7 +41,7 @@ if(isset($_SESSION['user'])){
             } else{ ?>
 <div class="panel panel-primary">
   
-  <div class="panel-heading">My ADs</div>
+  <div class="panel-heading text-center">ALL Items</div>
   <div class="panel-body">
       
   <?php if(! empty(getItem('user_id', $get['userId']))){
@@ -51,7 +51,7 @@ if(isset($_SESSION['user'])){
           <span class="price"> <?php echo $userAds['price'] ?></span>
           <img src="computer.png" class="img-fluid img-thumbnail" alt="Responsive image">
           <div class="caption">
-              <h3 class="name text-center">  <?php echo $userAds['name'] ?></h3>
+              <h3 class="name text-center"> <a href="item.php?do=item&id=<?php echo $userAds['item_id']; ?>"><?php echo $userAds['name'] ?></a> </h3>
               <p class="description text-center"><?php echo $userAds['description'] ?></p>
           </div>
       </div>
