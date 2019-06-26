2e1a13cb10999703c939d149b9751cd30e726517
Ahmed Ibrahim <0ahmedibrahem@gmail.com>
2019-06-26 18:41:40 +0200

    comments approve by admin and jus show approved

--- incloudes/functions/function.php
+++ incloudes/functions/function.php
@@ -1,6 +1,29 @@
 <?php
 
 /**
+ * function to get comments by status
+ * 
+ */
+function CommentsByStatus($status, $id_item)
+{
+    // $set = '';
+    // if ($status == 'active') {
+    //     $set = 1;
+    // } else {
+    //     $set = 0;
+    // }
+    global $con;
+    $sql = "SELECT comments.*,users.username, items.name FROM comments  INNER JOIN users ON users.userId = comments.user_id
+        INNER JOIN items ON items.item_id = comments.id_item  WHERE comments.id_item =:id AND comments.status =:status";
+    $query = $con->prepare($sql);
+    $query->bindParam(':status', $status, PDO::PARAM_INT);
+    $query->bindParam(':id', $id_item, PDO::PARAM_INT);
+    $query->execute();
+    $fetchAll = $query->fetchAll();
+    return $fetchAll;
+}
+
+/**
  * function to get any one data from any database by [id]
  * @par your selector in any table
  * @par any table from database
@@ -40,7 +63,7 @@ function getItem($where, $value){
     $query->bindParam(':id', $value,PDO::PARAM_INT);
     $query->execute();
     $items = $query->fetchAll();
-    
+
     return $items;
 }
 /**
@@ -49,14 +72,13 @@ function getItem($where, $value){
  * @return number [rowCount from database]  [1 (user is not active) || 0 (user is active) ]
  */
 function getStatus($user){
-global $con;
-$stmt = "SELECT username, regStatus FROM `users` WHERE username=:user AND regStatus=0";
-$query = $con->prepare($stmt);
-$query->bindParam(':user', $user, PDO::PARAM_STR);
-$query->execute();
-$count = $query->rowCount();
-return $count;
-
+    global $con;
+    $stmt = "SELECT username, regStatus FROM `users` WHERE username=:user AND regStatus=0";
+    $query = $con->prepare($stmt);
+    $query->bindParam(':user', $user, PDO::PARAM_STR);
+    $query->execute();
+    $count = $query->rowCount();
+    return $count;
 }
 
 /** ADMIN Functions */
@@ -79,7 +101,8 @@ function getTitle()
  * @return  (integer) number if true , zero if false [if data exists gives number if not exist gives zero]
  * 
  */
-function checkUsername($select, $from, $value) {
+function checkUsername($select, $from, $value)
+{
     global $con;
     $stmt = "SELECT $select FROM $from WHERE $select=:value";
     $query = $con->prepare($stmt);
@@ -87,7 +110,7 @@ function checkUsername($select, $from, $value) {
     $query->execute();
     $checkCount = $query->rowCount();
     return $checkCount;
- }
+}
 
 /**
  * redirect function
@@ -95,51 +118,50 @@ function checkUsername($select, $from, $value) {
  * @par secends before direct
  */
 
-  function myDirect($mesg, $url = null, $seconeds = 1){
+function myDirect($mesg, $url = null, $seconeds = 1){
     $var = '';
-      if ($url == null){
-          $var = 'index.php';
-      } elseif($url == 'back'){
-          $var = (isset($_SERVER['HTTP_REFERER'])&& $_SERVER['HTTP_REFERER'] != ''? $_SERVER['HTTP_REFERER'] : 'index.php');
+    if ($url ==  null){
+        $var = 'index.php';
+    } elseif($url == 'back'){
+        $var = (isset($_SERVER['HTTP_REFERER'])&& $_SERVER['HTTP_REFERER']  != ''? $_SERVER['HTTP_REFERER'] : 'index.php');
       }else{
         $var = $url;
-      }
+    }
     header("refresh:$seconeds;url=$var");
-     echo  $mesg;
+    echo  $mesg;
     exit();
-     
-  }
-  /**
-   * count Item
-   * @par column name
-   * @par table name
-   * @return integer
-   */
-  function countItem($column, $table,$where = null){
-      global $con;
-        $st = "SELECT COUNT($column) FROM $table".' '."$where ";
-        $query= $con->prepare($st);
-        $query->execute();
-        return $query->fetchColumn();
-  }
-  /**
-   * function to get latest update on database
-   * @par $select the row selector
-   * @par $table (column name) table which contain a selector example [* , userId, username]
-   * @par $order (column name) ordered by example [userId]  can use 'all' word to display all rows
-   * @par $limit ( integer ) result limit exapmle [3]
-   */
+}
+/**
+ * count Item
+ * @par column name
+ * @par table name
+ * @return integer
+ */
+function countItem($column, $table, $where = null){
+    global $con;
+    $st = "SELECT COUNT($column) FROM $table " .' '."$where ";
+    $query= $con->prepare($st);
+    $query->execute();
+    return $query->fetchColumn();
+}
+/**
+ * function to get latest update on database
+ * @par $select the row selector
+ * @par $table (column name) table which contain a selector example [* , userId, username]
+ * @par $order (column name) ordered by example [userId]  can use 'all' word to display all rows
+ * @par $limit ( integer ) result limit exapmle [3]
+ */
 
-   function latest($select = '*' , $table, $order = 'id', $limit = '3', $oredring = 'DESC'){
-       global $con;
-       if ($limit == 'all'){ // add limit all to function
-           $lim = '';
-       } else{
-           $lim = 'LIMIT ' . $limit;
-       }
-       $newStmt = "SELECT $select FROM $table ORDER BY $order $oredring ";
-       $query= $con->prepare($newStmt);
-        $query->execute();
-        $fetched = $query->fetchAll();
-        return $fetched;
-   }
+function latest($select = '*', $table, $order = 'id', $limit = '3', $oredring = 'DESC'){
+    global $con;
+    if ($limit == ' all'){ // add limit all to function
+        $lim = '';
+    }  else{
+        $lim = 'LIMIT ' . $limit;
+    }
+    $newStmt = "SELECT $select FROM $table ORDER BY $order $oredring ";
+    $query= $con->prepare($newStmt);
+    $query->execute();
+    $fetched = $query->fetchAll();
+    return $fetched;
+}
--- item.php
+++ item.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['user'])) {
 
         // $q = "SELECT * FROM `comments`  $queu";
         $sql = "SELECT comments.*,users.username, items.name FROM comments  INNER JOIN users ON users.userId = comments.user_id
-        INNER JOIN items ON items.item_id = comments.id_item  WHERE comments.id_item =:id";
+        INNER JOIN items ON items.item_id = comments.id_item  WHERE comments.id_item =:id AND .comments.status=1";
         $query = $con->prepare($sql);
         $query->bindparam(':id', $commentId, PDO::PARAM_INT);
         $update = $query->execute();
@@ -110,14 +110,33 @@ if (isset($_SESSION['user'])) {
                     ?>
                     <!-- End add comment -->
                     
-                    <hr class="custom-hr">
+                    
                     <!-- show comments -->
                     
 
-                            <?php if (isset($fetchComments)) {  // loop to catch all users information
+                            <?php // add comment do database 
+                               if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add-comment'])) {
+                                $ucomment = $_POST['textarea'];
+                                $userid = $_SESSION['user-id'];
+                                $itemid =  getDataById('item_id', 'items', 'item_id', $userAds['item_id']);
+                                if (!empty($ucomment) && isset($userid)) {
+                                    $q = "INSERT INTO `comments` ( user_id, id_item , date,  comment) VALUES (:user_id, :id_item, now(), :comment)";
+                                    $insert = $con->prepare($q);
+                                    $insert->bindparam(':user_id', $userid, PDO::PARAM_INT);
+                                    $insert->bindparam(':id_item', $itemid, PDO::PARAM_INT);
+                                    $insert->bindparam(':comment', $ucomment, PDO::PARAM_STR);
+                                    $insert->execute();
+                                   if($insert == true){
+                                       $msg = '<div class="alert alert-success text-center">Your comment is pending to admin</div>';
+                                       echo myDirect($msg, 'back', 2);
+                                   }
+                                }
+                            }   
+                                 // end add comments to database from user
+                                 // show comments 
+                            if (isset($fetchComments)) {  // loop to catch all users information
                                 foreach ($fetchComments as $comment) {
                                     ?>
-        
                     <div class="row comment-row">
                         <div class="col-md-3">
                             <div class="avatar ">
@@ -144,24 +163,8 @@ if (isset($_SESSION['user'])) {
                     <!-- End show comments -->
                 </div>
 
-            <?php // add comment do database 
-         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add-comment'])) {
-            $ucomment = $_POST['textarea'];
-            $userid = $_SESSION['user-id'];
-    
-            $itemid =  getDataById('item_id', 'items', 'item_id', $userAds['item_id']);
-            if (!empty($ucomment) && isset($userid)) {
-                $q = "INSERT INTO `comments` ( user_id, id_item , date,  comment) VALUES (:user_id, :id_item, now(), :comment)";
-                $insert = $con->prepare($q);
-                $insert->bindparam(':user_id', $userid, PDO::PARAM_INT);
-                $insert->bindparam(':id_item', $itemid, PDO::PARAM_INT);
-                $insert->bindparam(':comment', $ucomment, PDO::PARAM_STR);
-                $insert->execute();
-               if($insert == true){
-                   echo myDirect('done', 'back', 0);
-               }
-            }
-        }    
+            <?php 
+       
         }
     } else {
         echo 'there are no ads to show';
